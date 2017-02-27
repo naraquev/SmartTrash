b72fb01e154555a542d5d079dd38f0f9aff70b35
Nicolas Araque <nicolasaraque2@gmail.com>
2017-02-27 02:21:45 +0000

    Resuelto sin Def:

--- tra.py
+++ tra.py
@@ -14,6 +14,7 @@ http = urllib3.PoolManager()
 headers = urllib3.util.make_headers(user_agent=None)
 headers['Authorization'] = 'Bearer ' + config.oauth_credentials_for_device
 headers['Content-Type'] = 'application/json;charset=utf-8'
+url='https://iotmms' + config.hcp_account_id + config.hcp_landscape_host + '/com.sap.iotservices.mms/v1/api/http/data/' + str(config.device_id)
 
 
 '''****************************************************************************************
@@ -55,73 +56,36 @@ currentDistance = 0
 notificationSentTime = 0
 
 LOOP_SAMPLING_TIME = 2
-CRITICAL_DISTANCE = 50
+CRITICAL_DISTANCE = 100
 NOTIFICATION_TIME_DELAY = 15
 
 
 
-def distanceMeasurement():
-	try:
-		global client,deviceType,LOOP_SAMPLING_TIME,NOTIFICATION_TIME_DELAY,CRITICAL_DISTANCE,currentCriticalLevelFlag,criticalLevelChangeOverFlag
-		l_prev_distance = 0
-		ultrasonicSensorInit()
-		while 1:
-			if GPIO.input(LIDCOVER) == 0:
-				time.sleep(LOOP_SAMPLING_TIME)		
-				GPIO.output(TRIG, True)
-				time.sleep(0.00001)
-				GPIO.output(TRIG, False)
-				print(1)
-				#Starts the timer 
-				while GPIO.input(ECHO)==0:
-					pulse_start = time.time()
-				#Waits for the timer to end once the pin is high
-				while GPIO.input(ECHO)==1:
-					pulse_end = time.time()
-				pulse_duration = pulse_end - pulse_start
-				l_distance = pulse_duration * 17150
-				l_distance = round(l_distance, 2)
-				currentDistance = l_distance
-				
-				distanceRecordedTime = datetime.datetime.now()
-                body='{"mode":"async", "messageType":"' + str(config.message_type_id_From_device) + '", "messages":[{"timestamp":' + str(timestamp) + ', "Distance":"' + str(l_distance) + ', "ID":"' + str(1) +'"}]}'
-	
-                r = http.urlopen('POST', url, body=body, headers=headers)
-                print("send_to_hcp():" + str(r.status))
-                print(r.data)
-
-                timestamp=int(time.time())
-                if currentDistance < CRITICAL_DISTANCE:
-                    GPIO.output(alarmOut,True)
-                    if currentCriticalLevelFlag == False:#6.1.1
-                        currentCriticalLevelFlag = True#6.1.1.2
-                        criticalLevelChangeOverFlag = True#6.1.1.3
-                        criticalLevelReachedTime = datetime.datetime.now()
-                    else:
-                        criticalLevelChangeOverFlag = False
-                else:
-                    GPIO.output(alarmOut,False)					
-                    currentCriticalLevelFlag = False
-                    criticalLevelChangeOverFlag = False
-
-
-				
-
-				# This means that in this measurement loop the level stays at the critical level
-				
-				#if (currentCriticalLevelFlag == True):
-					#calculate timedifference
-				#	diff = distanceRecordedTime - notificationSentTime 
-	
-				#	day  = diff.days
-				#	hour = (day*24 + diff.seconds/3600)
-				#	diff_minutes = (diff.days *24*60)+(diff.seconds/60)			
-
-				#	if diff_minutes > NOTIFICATION_TIME_DELAY:
-				
-			
-						
-	except KeyboardInterrupt: 
+try:
+	start = 0
+	l_prev_distance = 0
+	ultrasonicSensorInit()
+	while 1:
+		if GPIO.input(LIDCOVER) == 0:
+			time.sleep(LOOP_SAMPLING_TIME)		
+			GPIO.output(TRIG, True)
+			time.sleep(0.00001)
+			GPIO.output(TRIG, False)
+			#Starts the timer 
+			while (GPIO.input(ECHO)==0):
+			    start = time.time()
+			#Waits for the timer to end once the pin is high
+			while GPIO.input(ECHO)==1:
+				end = time.time()
+			pulse_duration = end - start
+			l_distance = pulse_duration * 17150
+			l_distance = round(l_distance, 2)
+			currentDistance = l_distance
+			timestamp= time.time()
+			print(currentDistance)
+			body= '{"mode":"async", "messageType":"' + str(config.message_type_id_From_device) + '", "messages":[{"timestamp":' + str(timestamp) + '"'+ ', "distance":"' + str(l_distance) + '"'+ ', "ID":"' + str(1) +'"}]}'
+			r = http.urlopen('POST', url, body=body, headers=headers)
+			if currentDistance < 50:
+			    print("cuidado")
+except KeyboardInterrupt: 
 		GPIO.cleanup()
-	
-distanceMeasurement()
